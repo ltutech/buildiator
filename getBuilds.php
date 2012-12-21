ec2a40318b416ebaefb407a39590b42b90f72272
Adrien <achey@ltutech.com>
2012-12-21 15:42:52 +0100

    Jobs | Order by date Failed and Unstable Build for display
    Tests:

--- getBuilds.php
+++ getBuilds.php
@@ -8,23 +8,44 @@ function isort($a,$b) {
     return ($a['timeElapse']) > ($b['timeElapse']);
 }
 
+function displayJosProblem($jobs)
+{
+  $html = '';
+  foreach ($jobs as $job) {
+    $blame = null;
+    $claim = null;
+    $status = $job['status'];
+    if (!empty($job['blame'])) {
+      $culprit = $job['blame'];
+      array_push($job['status'], "$culprit blamed");
+      $blame = "<span class='blame'>{$job['blame']}</span>" ;
+    }
+    if (!empty($job['claim'])) {
+      $culprit = $job['claim'];
+      array_push($job['status'], "$culprit claimed");
+      $claim = "<span class='claim' >{$job['claim']}</span>" ;
+    }
+    $html .="<li class = 'jobBroken " . implode(" ",$job['status'] ) . "'>{$job['name']}${claim}{$blame}</li><li class = 'lastSuccedBuild '>{$job['lastSuccessfulBuildTime']}\n</li>";
+  }
+  return $html;
+}
+
 $result = '';
 if (isset($_GET['view'])) {
-	$ci = new JenkinsCI('http://continuousintegration.corp.ltutech.com', $_GET['view']);
+  $ci = new JenkinsCI('http://continuousintegration.corp.ltutech.com', $_GET['view']);
 } else {
-	$ci = new JenkinsCI('http://continuousintegration.corp.ltutech.com');
+  $ci = new JenkinsCI('http://continuousintegration.corp.ltutech.com');
 }
 
 try {
-	$jobs = $ci->getAllJobs();
+  $jobs = $ci->getAllJobs();
 } catch (BuildiatorCIServerCommunicationException $e) {
-	$result = array('status'  => 'error',
-					'content' => $e->getMessage());
+  $result = array('status'  => 'error',	'content' => $e->getMessage());
 }
 
 if (!is_array($result)) {
-	$html = '';
-  uksort($jobs, "isort");
+  $html = '';
+  usort($jobs, "isort");
 
   $jobsFailed = array();
   $jobsUnstable = array();
@@ -39,36 +60,22 @@ if (!is_array($result)) {
     }
     if ((($lsStatus == 'successful') or ($lsStatus == 'cancelled')) or ($lsStatus == 'disabled')) {
       $jobsStable[] = $job;
-      }
+    }
   }
 
-  foreach ($jobsFailed as $job) {
- 		$blame = null;
- 		$claim = null;
-		$status = $job['status'];
-		if (!empty($job['blame'])) {
-			$culprit = $job['blame'];
-			array_push($job['status'], "$culprit blamed");
-			$blame = "<span class='blame'>{$job['blame']}</span>" ;
-		}
-		if (!empty($job['claim'])) {
-			$culprit = $job['claim'];
-			array_push($job['status'], "$culprit claimed");
-			$claim = "<span class='claim' >{$job['claim']}</span>" ;
-		}
-		if ($job['status'][0] != 'successful' ) {
-				$html .="<li class = 'jobBroken " . implode(" ",$job['status'] ) . "'>{$job['name']}${claim}{$blame}</li><li class = 'lastSuccedBuild '>{$job['lastSuccessfulBuildTime']}\n</li>";
-    }
-	}
+  $html .= displayJosProblem($jobsFailed);
+  $html .= displayJosProblem($jobsUnstable);
 
-  $html .="<li class = 'jobsBorderHeader '>SuccessfullBuild</li>";
+  $countJobs = count($jobs);
+  $countJobsStable = count($jobsStable);
+  $html .="<li class = 'jobsBorderHeader '>Successfull Build: $countJobsStable/$countJobs</li>";
   $html .="<p>=============================</p>";
 
   foreach ($jobsStable as $job) {
-		$html .="<li class = 'jobSuccess " . implode(" ",$job['status'] ) . "'>{$job['name']}${claim}{$blame}</li>";
-	}
+    $html .="<li class = 'jobSuccess " . implode(" ",$job['status'] ) . "'>{$job['name']}</li>";
+  }
 
-	$result = array('status' => 'ok', 'content' => $html);
+  $result = array('status' => 'ok', 'content' => $html);
 }
 
 echo json_encode($result);
